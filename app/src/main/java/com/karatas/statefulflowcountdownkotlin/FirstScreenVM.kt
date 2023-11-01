b59c40cf9e25bcb36f104660cc3646ca948890b4
Vahdet Karatas <38797434+vahdetkaratas@users.noreply.github.com>
2023-11-01 23:38:32 +0200

    first commit

---
+++ app/src/main/java/com/karatas/statefulflowcountdownkotlin/FirstScreenVM.kt
@@ -0,0 +1,27 @@
+package com.karatas.statefulflowcountdownkotlin
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
+class FirstScreenVM : ViewModel() {
+
+    //this whole thing wouldn't matter if we was working on a normal stateflow
+    //private val _mycounter = MutableStateFlow(0)
+    //val mycounter = _mycounter.asStateFlow()
+
+    private var count = 0
+
+    val counter = flow{
+        while (true) {
+            delay(1000L)
+            println("running flow")
+            emit(count++)
+        }
+    }
+        //.stateIn(viewModelScope, SharingStarted.Lazily,0)
+        //we can share the flow to be emitted by multiple observers
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000),0)
+    //we give 5 seconds to run for flows for cases such as rotating the screen or minimizing the app
+}
---
+++ app/src/main/java/com/karatas/statefulflowcountdownkotlin/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.karatas.statefulflowcountdownkotlin
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.karatas.statefulflowcountdownkotlin.ui.theme.StatefulFlowCountDownKotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StatefulFlowCountDownKotlinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "firstScreen"){
+                        composable(route = "firstScreen") {
+                            val viewModel = viewModel<FirstScreenVM>()
+                            //val time by viewModel.counter.collectAsState()
+                            //collectAsState works when we convert a regular flow into a stateful one
+                            //however it does not have any info on lifecycle so it will continue when you minimize the app
+                            val time by viewModel.counter.collectAsStateWithLifecycle()
+
+                            FirstScreen(
+                                time = time,
+                                onButtonClicked = {
+                                    navController.navigate("secondScreen")
+                                }
+                            )
+
+                        }
+
+                        composable("secondScreen") {
+                            SecondScreen()
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
